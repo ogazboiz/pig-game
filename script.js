1a6ff8d5b3cdcbcf2f47aa3945ad91d88fc37752
ogazboiz <ogazboizakpolo@gmail.com>
2023-01-13 02:44:16 +0100

    initial stage

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 'use strict';
+const player_0 = document.querySelector('.player--0');
+const player_1 = document.querySelector('.player--1');
 const scoreEl_0 = document.querySelector('#score--0');
 const scoreEl_1 = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
@@ -8,15 +10,36 @@ const currentscore_0 = document.getElementById('current--0');
 const currentscore_1 = document.getElementById('current--1');
 
 //initial score
+
 scoreEl_0.textContent = 0;
 scoreEl_1.textContent = 0;
 diceEl.classList.add('hidden');
 let score = 0;
-
+let totalScore = [0, 0];
+let activePlayer = 0;
+//switch player
+let switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  score = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player_0.classList.toggle('player--active');
+  player_1.classList.toggle('player--active');
+};
 btnDice.addEventListener('click', function () {
   diceEl.classList.remove('hidden');
   let diceNumber = Math.trunc(Math.random() * 6 + 1);
   diceEl.src = `images/dice-${diceNumber}.png`;
-  score = score + diceNumber;
-  currentscore_0.textContent = score;
+  if (diceNumber !== 1) {
+    score = score + diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent = score;
+  } else {
+    switchPlayer();
+  }
+});
+btnHold.addEventListener('click', function () {
+  totalScore[activePlayer] = totalScore[activePlayer] + score;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    totalScore[activePlayer];
+
+  switchPlayer();
 });
